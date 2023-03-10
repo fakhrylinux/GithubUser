d066dcadb260ac8c620c866984590bb7dce94f63
Fakhry <fakhry.linux@gmail.com>
2023-03-11 05:48:40 +0800

    Initial commit

---
+++ app/src/main/java/me/fakhry/githubuser/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.fakhry.githubuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
